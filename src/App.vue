2d4a2498e1517bc9ff246456c4ee9fcdc71a8913
Karl Heitmann <khf1988@gmail.com>
2022-04-24 16:58:29 -0400

    creado un NavBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup>
-</script>
-
 <template>
+  <NavBar />
   <RouterLink to="/" class="button">Notes</RouterLink>
   <RouterLink to="/stats" class="button">Stats</RouterLink>
   <RouterView />
 </template>
+
+<script setup>
+/*
+  imports
+*/
+  import NavBar from '@/components/layout/NavBar.vue'
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
